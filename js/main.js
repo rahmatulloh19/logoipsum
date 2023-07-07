88e0246adca1bea0af02ef1fdd5b66b6770d773f
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2023-07-07 11:04:47 +0500

    prapere hero section

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var elHeader = document.querySelector(".site-header");
+var elMenuButton = document.querySelector(".site-header__menu");
+var elModeButton = document.querySelector(".site-header__mode");
+
+elMenuButton.addEventListener("click", () => {
+  elHeader.classList.toggle("open");
+  if(elHeader.classList.contains("open")) {
+    elHeader.classList.add("menu-color");
+    document.body.style.overflowY = "hidden";
+  } else {
+    elHeader.classList.remove("menu-color")
+    document.body.removeAttribute("style");
+  }
+})
+
+var elBody = document.body;
+
+elModeButton.addEventListener("click", () => {
+  elBody.classList.toggle("dark-mode");
+})
