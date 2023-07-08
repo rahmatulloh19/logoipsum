2b1619b2de381b8ed8531927fe85f590ff9f1e0f
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2023-07-08 11:57:13 +0500

    prepare section price

--- js/main.js
+++ js/main.js
@@ -18,3 +18,25 @@ var elBody = document.body;
 elModeButton.addEventListener("click", () => {
   elBody.classList.toggle("dark-mode");
 })
+
+$(document).ready(function(){
+  
+$(".customer__list").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+  ]
+});
+});
