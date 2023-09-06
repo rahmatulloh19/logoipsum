653378484fc0a962c09454847c4a3c1e128c0663
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2023-09-06 07:41:11 +0500

    romove comments in main.js file

--- js/main.js
+++ js/main.js
@@ -30,27 +30,3 @@ elModeButton.addEventListener("click", () => {
     localStorage.setItem("mode", JSON.stringify(false))
   }
 })
-
-
-
-// $(document).ready(function(){
-  
-//   $(".customer__list").slick({
-//     dots: true,
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//           infinite: true,
-//           dots: true
-//         }
-//       }
-//     ]
-//   });
-//   });
